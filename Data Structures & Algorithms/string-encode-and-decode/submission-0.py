c24a4e102c8219139e08530b3a965aa4edc91632
Kevin Pierce Ramos <82022733+Kemoshu@users.noreply.github.com>
2026-06-29 11:46:16 -0700

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        final = ""
+        for i in strs:
+            final += str(len(i))
+            final += '#'
+            final += i
+        return final
+    def decode(self, s: str) -> List[str]:
+        myStack = []
+        final = []
+        i = 0
+        while i < len(s):
+            if s[i] != '#':
+                myStack.append(s[i])
+                i += 1
+            else: 
+                strlen = ""
+                while len(myStack) > 0:
+                    chars = myStack.pop()    
+                    strlen += str(chars)
+                strlen = int(strlen[::-1])
+                final.append(s[i + 1 : i + strlen + 1])
+                print(s[i + 1 : i + strlen + 1])
+                i += strlen + 1
+        return final
+
